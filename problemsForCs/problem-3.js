61da4df85ca9a2d4eb0455aea9a345f532acbf80
Abdullah149081 <masudmd425@gmail.com>
2023-02-04 23:06:59 +0600

    How many numbers are greater than five digits

---
+++ problemsForCs/problem-3.js
@@ -0,0 +1,15 @@
+function ratherThenFive(arrayNumber) {
+  let storeArray = [];
+  for (var i = 0; i < arrayNumber.length; i++) {
+    let getElement = arrayNumber[i];
+    if (getElement > 5) {
+      storeArray.push(getElement);
+    }
+  }
+
+  return storeArray.length;
+}
+
+const array = [-1, 2, -3, 4, 5, 6, -7, 8, -9, 10];
+const output = ratherThenFive(array);
+console.log(output);
