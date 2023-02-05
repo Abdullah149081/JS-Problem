1f1367a201c8399452fefc210d1c2f281fd9e99b
Abdullah149081 <masudmd425@gmail.com>
2023-02-05 12:11:44 +0600

    update Js-Problem Problem-2

--- problem-2.js
+++ problem-2.js
@@ -1,24 +1,17 @@
-let num1 = 10;
+let num1 = 14;
 let num2 = 12;
 
 if (num1 < num2) {
   console.log("Number Two: is big");
-} 
+} else if (num1 != num2) {
+  console.log("Number one and number Two not Equal");
+  if (num2 < num1) {
+    console.log("Number Two is lees than Number One / Number Two is Small");
 
-else if( num1 != num2 ){
-    console.log("Number one and number Two not Equal")
-     if (num2 < num1) {
-        console.log("Number One: is big");
-      
-        if (num1 > num2) {
-          console.log("Number two is small");
-        }
-      }
+    if (num1 > num2) {
+      console.log("Number One is Greater than Number Two / Number One Is Big");
+    }
+  }
+} else if (num1 == num2) {
+  console.log("Number one and Number Tow Equal");
 }
-
-
-
-else if( num1 == num2){
-    console.log("Number one and Number Tow Equal")
-}
-
