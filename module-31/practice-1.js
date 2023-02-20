a83d52e781589e0b9b1b431a8ab97a02db077aa4
Abdullah149081 <masudmd425@gmail.com>
2023-02-20 11:39:06 +0600

    Template String

--- module-31/practice-1.js
+++ module-31/practice-1.js
@@ -2,3 +2,13 @@ const multiply = (n, n1, n2) => n * n1 * n2;
 
 const result = multiply(5, 5, 8);
 console.log(result);
+
+/* 
+2) Write the following sentence in three lines and print the result: I am a web developer. I love to code. I love to eat biryani.  */
+
+const templateString = `I am a web developer.
+I love to code.
+I love to eat vitamin Food  
+`;
+
+console.log(templateString);
