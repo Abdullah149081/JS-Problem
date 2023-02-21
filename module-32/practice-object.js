6e3ccfcf8f245db39e662d9954155d806ffcb7a9
Abdullah149081 <masudmd425@gmail.com>
2023-02-21 20:13:47 +0600

    object valu get

---
+++ module-32/practice-object.js
@@ -0,0 +1,16 @@
+const student = {
+  name: "masud",
+  age: 24,
+};
+
+console.log(student.age);
+let data = {
+  location: [
+    {
+      city: "dhaka",
+      country: "bangladesh",
+    },
+  ],
+};
+
+console.log(data.location[0].city);
