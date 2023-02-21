74ecb521a8f382b6fd9879e54e34dbddc1e48ee6
Abdullah149081 <masudmd425@gmail.com>
2023-02-21 20:05:43 +0600

    object reduce

--- module-32/practice-reduce.js
+++ module-32/practice-reduce.js
@@ -17,3 +17,27 @@ const reduceMethod = forMethod.reduce((previousNumber, currentNumber) => {
 }, 0);
 
 console.log(reduceMethod);
+
+/*  Follow above array of objects. So, you have 3 objects as  array element. Can you find out the total sum from here?  
+20 + 15 + 22 = 57 . The output will be 57 
+What are you thinking? Yeah! Do it with for loop. I know you can do it.  
+But! Wait !! Wait !!! Do the same task using array.reduce() method.  
+ */
+
+const people = [
+  { name: "masud", age: 20 },
+  { name: "masud", age: 15 },
+  { name: "masud", age: 22 },
+];
+
+let sumAge = 0;
+
+for (const peopleAge of people) {
+  sumAge = sumAge + peopleAge["age"];
+}
+
+console.log(sumAge);
+
+const answer = people.reduce((previousValue, CurrentValue) => previousValue + CurrentValue.age, 0);
+
+console.log(answer);
